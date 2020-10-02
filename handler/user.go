fdcdefb061baeeebef17c91934666346f7509c95
vndee <hvd.huynhduy@gmail.com>
2020-10-02 16:19:18 +0700

    add streaming api

--- handler/user.go
+++ handler/user.go
@@ -1,9 +1,59 @@
 package handler
 
 import (
+	"encoding/json"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"time"
 )
+
+type User struct {
+	Name string
+	Age  int
+}
+
+var listUsers = []User{
+	{
+		Name: "A",
+		Age:  1,
+	},
+	{
+		Name: "B",
+		Age:  2,
+	},
+	{
+		Name: "C",
+		Age:  3,
+	},
+	{
+		Name: "D",
+		Age:  4,
+	},
+	{
+		Name: "E",
+		Age:  5,
+	},
+	{
+		Name: "F",
+		Age:  6,
+	},
+	{
+		Name: "G",
+		Age:  7,
+	},
+	{
+		Name: "H",
+		Age:  8,
+	},
+	{
+		Name: "I",
+		Age:  9,
+	},
+	{
+		Name: "J",
+		Age:  10,
+	},
+}
 
 func GetUser(c echo.Context) error {
 	return c.String(http.StatusOK, "api get user")
@@ -15,4 +65,22 @@ func UpdateUser(c echo.Context) error {
 
 func DeleteUser(c echo.Context) error {
 	return c.String(http.StatusOK, "api delete user")
+}
+
+func GetAllUser(c echo.Context) error {
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	c.Response().WriteHeader(http.StatusOK)
+
+	enc := json.NewEncoder(c.Response())
+
+	for _, user := range listUsers {
+		if err := enc.Encode(user); err != nil {
+			return err
+		}
+
+		c.Response().Flush()
+		time.Sleep(1 * time.Second)
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -18,13 +18,14 @@ func main() {
 	server.POST("/login", handler.Login, middleware.BasicAuth(middlewares.BasicAuth))
 	server.GET("/admin", handler.Hello, isLoggedIn, isAdmin)
 
-	groupv2 := server.Group("/v2")
+	groupv2 := server.Group("/v2", isLoggedIn)
 	groupv2.GET("/hello", handler.Hello2)
 
 	groupUser := server.Group("/api/user")
 	groupUser.GET("/get", handler.GetUser)
-	groupUser.GET("/update", handler.UpdateUser)
-	groupUser.GET("delete", handler.DeleteUser)
+	groupUser.GET("/update", handler.UpdateUser, isAdmin)
+	groupUser.GET("/delete", handler.DeleteUser, isAdmin)
+	groupUser.GET("/get_all", handler.GetAllUser)
 
 	server.Logger.Fatal(server.Start(":8888"))
 }
